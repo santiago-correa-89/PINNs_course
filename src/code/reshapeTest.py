6352123227aa4b80138c415011bc91d21774c061
Leopoldo Agorio <leopoldoagorio@gmail.com>
2022-11-25 13:58:13 -0300

    defined functions for easy migration

--- src/code/reshapeTest.py
+++ src/code/reshapeTest.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy import spatial
 from pyDOE import lhs
 
-def select_points(Xdata,N_u,criterion='fem'):
+def select_idx(Xdata,N_u,criterion='fem'):
     xmin = -5
     xmax = 15
     ymin = -5
@@ -25,9 +25,26 @@ def select_points(Xdata,N_u,criterion='fem'):
         _, idx = spatial.KDTree(Xdata).query(samps) 
     return idx
 
+def conform_data(Xdata, Udata, idx, T=None):
+    if T==None:
+        T = Udata.shape[-1]
+    Xsort = Xdata[idx,:]
+    X_train = np.c_[np.vstack([Xsort]*T),np.array(list(range(T))).repeat(N_u)]
+
+    Usort = Udata[idx,:]
+    U_train = np.vstack([Usort[:,:,t] for t in range(T)]) 
+    return X_train, U_train
+
+def circle_points(N_f):
+    r = 0.5
+    angle = np.pi* np.random.uniform(0,2,N_f)
+    X_train_f = np.vstack([r*np.cos(angle),r*np.sin(angle)]).T
+    return X_train_f
+
 Xdata = np.load(r"src/data/VORT_DATA_VTU/Xdata.npy")
 Udata = np.load(r"src/data/VORT_DATA_VTU/Udata.npy")
 
+""" version without function definitions
 N_u = 100 #num of random samples to take
 T = Udata.shape[-1] # T = 201 
 T = 5 #test low dim. comment later
@@ -42,8 +59,21 @@ X_train = np.c_[np.vstack([Xsort]*T),np.array(list(range(T))).repeat(N_u)] #add 
 Usort = Udata[idx,:,:T]
 U_train = np.vstack([Usort[:,:,t] for t in range(T)]) #unroll vertically
 #Usort = np.vstack([Usort[:,:,t] for t in range(T)]) #unroll vertically
+N_f = 1500
+angle = np.pi* np.random.uniform(0,2,N_f)
+r = 0.5
+X_train_f = np.vstack([r*np.cos(angle),r*np.sin(angle)]).T
+ """
+N_u = 100
+N_f = 1500
+idx = select_idx(Xdata, N_u, criterion='lhs')
+X_d_train, U_d_train = conform_data(Xdata, Udata, idx)
+X_f_train = circle_points(N_f)
+
 print("i")
-plt.scatter(Xsort[:,0],Xsort[:,1])
+plt.scatter(Xdata[idx,0],Xdata[idx,1])
+plt.scatter(X_f_train[:,0],X_f_train[:,1],c='red')
+plt.legend(['Datos', 'Física'])
 plt.xlim([-5, 15])
 plt.ylim([-5, 5])
 
